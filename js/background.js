c67a1eeec5dee429531ab4e19842f23a51454d38
jmeno1011 <jwhltn8282@gmail.com>
2022-04-23 21:49:58 +0900

    img까지 완료

--- js/background.js
+++ js/background.js
@@ -1,9 +1,17 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "emotion-happy-svgrepo-com.svg",
+  "emotion-normal-svgrepo-com.svg",
+  "emotion-unhappy-svgrepo-com.svg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const right = document.getElementById("right");
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+bgImage.style.background = `url(img/${chosenImage}) no-repeat`;
+
+right.appendChild(bgImage);
